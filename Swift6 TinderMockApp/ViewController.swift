442e3cc0e1da13f5588d955646379313faab5843
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-01-30 17:53:01 +0900

    Initial Commit

---
+++ Swift6 TinderMockApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift6 TinderMockApp
+//
+//  Created by 平林 宏淳 on 2021/01/30.
+//  Copyright © 2021 Hiroaki Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift6 TinderMockAppTests/Swift6_TinderMockAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift6_TinderMockAppTests.swift
+//  Swift6 TinderMockAppTests
+//
+//  Created by 平林 宏淳 on 2021/01/30.
+//  Copyright © 2021 Hiroaki Hirabayashi. All rights reserved.
+//
+
+import XCTest
+@testable import Swift6_TinderMockApp
+
+class Swift6_TinderMockAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
